9c767900ccb98ee177a525eac3eeb1d75b1e2997
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-09 00:55:02 -0500

    agregacion del microservicio de courses al proyecto padre

---
+++ microservices-courses/src/main/java/com/microservices/courses/MicroservicesCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesCoursesApplication.class, args);
+	}
+
+}
