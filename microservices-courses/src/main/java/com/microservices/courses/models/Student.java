fecaff81e86024f4f435a3627a4ae53b137bc082
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-09 01:23:33 -0500

    entidad estudiante mscv-student

---
+++ microservices-courses/src/main/java/com/microservices/courses/models/Student.java
@@ -0,0 +1,31 @@
+package com.microservices.courses.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer estudentID;
+
+    @Column(nullable = false, length = 50)
+    private String firstName;
+
+    @Column(nullable = false, length = 50)
+    private String lastName;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 50)
+    private Long courseID;
+
+}
