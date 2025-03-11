c652843a83d9da5c104c7a583ee9c544c139612b
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-10 23:15:46 -0500

    anotacion de @EnableFeignClients  en la clase main del microservicio de cursos para que pueda llamar a otros microservicios

--- microservices-courses/src/main/java/com/microservices/courses/MicroservicesCoursesApplication.java
+++ microservices-courses/src/main/java/com/microservices/courses/MicroservicesCoursesApplication.java
@@ -3,13 +3,15 @@ package com.microservices.courses;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MicroservicesCoursesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroservicesCoursesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesCoursesApplication.class, args);
+    }
 
 }
