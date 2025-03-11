e3c93cdc9f0bc140c93a27616763fa0f6ca554a9
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-10 21:41:12 -0500

    entidad curso

---
+++ microservices-courses/src/main/java/com/microservices/courses/models/Course.java
@@ -0,0 +1,27 @@
+package com.microservices.courses.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long courseId;
+
+    @Column(nullable = false, length = 60)
+    private String courseName;
+
+    @Column(nullable = false, length = 60)
+    private String teacherName;
+
+}
