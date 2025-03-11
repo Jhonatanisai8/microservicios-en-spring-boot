e91b5e2e44bd0a7e8fbd465a933e49ec168c4084
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-11 15:35:22 -0500

    datos de prueba para las tablas de curso y estudiantes

---
+++ microservices-courses/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO courses (course_name, teacher_name) VALUES ('Matemáticas Avanzadas', 'Carlos López');
+INSERT INTO courses (course_name, teacher_name) VALUES ('Historia Universal', 'María Rodríguez');
+INSERT INTO courses (course_name, teacher_name) VALUES ('Programación en Java', 'Juan Pérez');
+INSERT INTO courses (course_name, teacher_name) VALUES ('Física Cuántica', 'Ana Gómez');
+INSERT INTO courses (course_name, teacher_name) VALUES ('Diseño de Bases de Datos', 'Diego Fernández');
